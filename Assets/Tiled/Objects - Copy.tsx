2100c786f4cb49fa553e0e74de4637cbaf116c5f
Haelgi <rataroleg@gmail.com>
2024-01-05 00:23:14 +0200

    remove assets

--- Assets/Tiled/Objects - Copy.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Objects - Copy" tilewidth="16" tileheight="16" tilecount="30" columns="6">
- <image source="../../../Downloads/Shining Fields_free_v1.1/Shining Fields_free_v1.1/Sprites/Objects/Objects - Copy.png" width="100" height="80"/>
-</tileset>
--- Assets/Tiled/spr_grass_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spr_grass_tileset" tilewidth="16" tileheight="16" tilecount="870" columns="30">
- <image source="../../../Downloads/Pixelarium - GrassLands - Free Version/Pixelarium - GrassLands - Free Version/Pack content/Sprites/Tileset/spr_grass_tileset.png" width="480" height="464"/>
-</tileset>
