a4865b6104c2eba5e91e8a4f178a519c77f352e0
Haelgi <rataroleg@gmail.com>
2024-01-05 21:47:32 +0200

    Revert "remove assets"
    This reverts commit 2100c786f4cb49fa553e0e74de4637cbaf116c5f.

---
+++ Assets/Tiled/Character_Run.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Character_Run" tilewidth="16" tileheight="16" tilecount="18" columns="6">
+ <image source="../../../Downloads/Dan's [16x16] Character and Tileset FREE/Characters/Character_Run.png" width="96" height="48"/>
+</tileset>
---
+++ Assets/Tiled/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="16" tileheight="16" tilecount="30" columns="6">
+ <image source="../../../Downloads/Shining Fields_free_v1.1/Shining Fields_free_v1.1/Sprites/Objects/Objects.png" width="100" height="80"/>
+</tileset>
