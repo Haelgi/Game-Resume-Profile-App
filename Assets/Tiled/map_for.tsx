de148ff7a9313f399f1b7d9684484e6960d6f01c
Haelgi <rataroleg@gmail.com>
2024-01-05 21:53:52 +0200

    del asset

--- Assets/Tiled/map_for.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="map_for" tilewidth="16" tileheight="16" tilecount="5400" columns="90">
- <image source="../sources/img/world/map_for.png" width="1440" height="960"/>
-</tileset>
